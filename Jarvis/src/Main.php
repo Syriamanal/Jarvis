9fcb4af2fdf5a02873e293f138b2cd0eb64dc25e
Syriamanal <shadinachat123@gmail.com>
2015-05-07 20:32:02 -0700

    not done
    it has a error the error is

--- Jarvis/src/Main.php
+++ Jarvis/src/Main.php
@@ -1,21 +1,40 @@
 <?php
-namespace Jarvis\Main.php
+namespace Jarvis\Main;
 
-use pocketmine\event\Listener;
+//Player
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\Player;
+//Other Stuff
+use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat; 
+use pocketmine\Server;
+//Text 
+use pocketmine\utils\TextFormat;
+//Commands
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 
 class Main extends PluginBase implements Listener {
+	
 	public function onEnable (){
-		$this->getLogger()->info(TextFormat::RED .  "<Jarvis>" . "I Have Indeed Loaded");
+		$this->getLogger()->info(TextFormat::RED . "<Jarvis> I Have Indeed Been Loaded");
 	}
 	
 	public function onPlayerJoin (PlayerJoinEvent $event){
 		$p = $event->getPlayer();
-		$p->sendMessage("<Jarvis>" . "Hello! I And Jarvis I Will Be Helping You Whenever You Need Help!");
+		$p->sendMessage(TextFormat::DARK_GREEN . "<Jarvis> Hello! I Am Jarvis I Will Be Helping You Whenever You Need Help!");
 		
 	}
 	
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args)
+	{
+		if($command->getName() == "Jarvis"){
+			$this->getLogger()->info(TextFormat::RED . "<Jarvis> What Can I Do For You Sir?");
+		}
+		else 
+		{
+			$this->getLogger()->info(TextFormat::RED . "<Jarvis> Sir That Is A Wrong Command!");
+		}
+	}
 }
